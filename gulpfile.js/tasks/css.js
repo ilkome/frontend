eba9d1a3a4615726c2ef6db72bf9cd508c6a0489
ilkome <ilya.komichev@gmail.com>
2016-02-18 12:52:19 -0800

    Add info about CSS files to console

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -3,15 +3,19 @@
 // Modules
 // ===============================================
 var gulp = require('gulp'),
+	paths = require('../paths'),
 	gutil = require('gulp-util'),
 	browserSync = require("browser-sync"),
-	paths = require('../paths');
+	debug = require('gulp-debug');
 
 
 // Copy CSS to build folder
 // ===============================================
 gulp.task('css', function() {
 	return gulp.src(paths.css.input)
+
+	// Show name of file in pipe
+	.pipe(debug({title: 'css:'}))
 
 	// Save files
 	.pipe(gulp.dest(paths.css.output))
