6d1af424485cddc0113982918aa3fa17b80848a4
ilkome <ilya.komichev@gmail.com>
2016-03-11 02:13:45 -0800

    Update setting.js

--- gulpfile.js/setting.js
+++ gulpfile.js/setting.js
@@ -9,5 +9,35 @@ module.exports = {
 		logFileChanges: false,
 		notify: false,
 		online: false
+	},
+	prettyHTML: {
+		html: {
+			braceStyle: "collapse",
+			indentChar: "\t",
+			indentScripts: "keep",
+			indentSize: 1,
+			maxPreserveNewlines: 10,
+			preserveNewlines: true,
+			unformatted: ["p", "title", "sub", "sup", "b", "i", "u"],
+			wrapLineLength: 0,
+			breakChainedMethods: false
+		},
+	},
+	jade: {
+		pretty: {
+			pretty: '\t'
+		},
+		minify: {
+			pretty: false
+		}
+	},
+	stylus: {
+		pretty: {
+			'compress': false,
+			'include css': false
+		},
+		minify: {
+			'compress': true
+		}
 	}
 }
