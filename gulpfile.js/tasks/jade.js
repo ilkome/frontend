7939e699f5bc8ad1541146188610747fdf4fb49a
ilkome <ilya.komichev@gmail.com>
2016-03-11 02:12:43 -0800

    Update tasks/jade.js
    Add settings

--- gulpfile.js/tasks/jade.js
+++ gulpfile.js/tasks/jade.js
@@ -4,6 +4,7 @@
 // ===============================================
 var gulp  = require('gulp'),
 	paths = require('../paths'),
+	settings = require('../setting'),
 	jade = require('gulp-jade'),
 	jadeInheritance = require('gulp-jade-inheritance'),
 	browserSync = require('browser-sync'),
@@ -13,27 +14,16 @@ var gulp  = require('gulp'),
 	flatten = require('gulp-flatten'),
 	plumber = require('gulp-plumber'),
 	debug = require('gulp-debug'),
-	gutil = require('gulp-util');
-
-
-// Settings
-// ===============================================
-var config = {
-	pretty: {
-		pretty: true
-	},
-	minify: {
-		pretty: false
-	}
-};
+	gutil = require('gulp-util'),
+	prettify = require('gulp-jsbeautifier');
 
 
 // Compile jade
 // ===============================================
 gulp.task('jade', function() {
-	return gulp.src(paths.jade.input)
+	return gulp.src(paths.pages.input)
 
-	// Error in syntax
+	// Error
 	.pipe(plumber(function(error) {
 		gutil.log(
 			gutil.colors.magenta('jade'),
@@ -43,41 +33,31 @@ gulp.task('jade', function() {
 		this.emit('end');
 	}))
 
-	// Keeps an in-memory cache of files
-	.pipe(cached('jadecache'))
+	// Show jade title in console
+	.pipe(debug({title: 'Jade:'}))
 
-	// Only pass through changed files
-	.pipe(changed(paths.build, {extension: '.html'}))
-
-	/**
-	 * Rebuild a jade file with other files
-	 * that have extended or included those file
-	 */
-	.pipe(jadeInheritance({basedir: paths.app}))
-
-	// Filter components
-	.pipe(gulpFilter(function (file) {
-		return !/\/components/.test(file.path);
-	}))
-
-	// Jade
-	.pipe(debug({title: 'jade:'}))
-	.pipe(jade(gutil.env.pretty ? config.pretty : config.minify))
+	// Prettify
+	.pipe(jade(gutil.env.minify ? settings.jade.minify : settings.jade.pretty))
+	
+	// TO DO: check
+	.pipe(prettify(settings.prettyHTML))
 
 	// Remove structure of folders
 	.pipe(flatten())
 
+	// Save files
 	.pipe(gulp.dest(paths.build))
 
 	.on('end', function() {
 		if(gutil.env.pretty) {
-			gutil.log(gutil.colors.magenta('jade'), ':', gutil.colors.green('pretty'));
+			gutil.log(gutil.colors.magenta('Jade'), ':', gutil.colors.green('pretty'));
 		} else {
-			gutil.log(gutil.colors.magenta('jade'), ':', gutil.colors.green('minify'));
+			gutil.log(gutil.colors.magenta('Jade'), ':', gutil.colors.green('minify'));
 		}
 
-		gutil.log(gutil.colors.magenta('jade'), ':', gutil.colors.green('finished'));
+		gutil.log(gutil.colors.magenta('Jade'), ':', gutil.colors.green('finished'));
 		gutil.log(gutil.colors.magenta('browserSync'), ':', gutil.colors.green('reload'));
+		gutil.log('----------------------------');
 		browserSync.reload();
 	})
 });
