6d4d2c5671f5edadc7ce1cc180e9f3bd741fd3d5
ilkome <ilya.komichev@gmail.com>
2016-04-09 21:04:40 -0700

    Update build 16.04.09

--- build/js/app.js
+++ build/js/app.js
@@ -1,8 +1,104 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", DOMready);
+// Import
+// =================================================================================================
+// import $ from 'jquery.min'
 
-function DOMready() {
-	// Print text
-	document.getElementById("js-app").innerHTML = "This text come from app.js";
-};
+// Preloader
+// =================================================================================================
+
+function preloader() {
+  $('.js-preloader').addClass('js-preloader-hidden');
+  $('.js-preloader-overflow').removeClass('js-preloader-overflow');
+}
+
+// Fix header jumping on mobile browsers
+// =================================================================================================
+function fixHeaderHeight(className) {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (userAgent.match(/Android/i) || userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
+    var block = $(className);
+    var blockHeight = block.outerHeight();
+
+    block.css('height', blockHeight);
+    block.css('min-height', 'auto');
+  }
+}
+
+// Run animation with reveal plugin
+// =================================================================================================
+function initAnimation() {
+  var sr = ScrollReveal({
+    viewFactor: 0.3
+  });
+
+  // Header avatar
+  sr.reveal('.js-headerAvaAnim', {
+    origin: 'top',
+    distance: '50px',
+    duration: 1500
+  });
+
+  // Header text
+  sr.reveal('.js-headerTextAnim', {
+    origin: 'center',
+    distance: '0',
+    duration: 1500,
+    delay: 1000
+  });
+
+  // Header text
+  sr.reveal('.js-headerSocialAnim', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1500,
+    delay: 2000
+  });
+
+  // Skills
+  sr.reveal('.js-skillAnim', {
+    origin: 'top',
+    distance: '50px',
+    duration: 1000
+  }, 300);
+
+  // Portfolio
+  sr.reveal('.js-portfolioAnim', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    delay: 300
+  });
+
+  // Contacts
+  sr.reveal('.js-contactsAnim', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000
+  });
+}
+
+// DOM ready
+// =================================================================================================
+$(function () {
+  // Remmove preloader
+  setTimeout(preloader, 5000);
+
+  // Fix header height on mobile devices
+  fixHeaderHeight('.js-header-in');
+
+  new Blazy({
+    selector: '.js-belazy',
+    offset: 300,
+    successClass: 'js-belazy-loaded'
+  });
+});
+
+// Everything ready
+// =================================================================================================
+$(window).load(function () {
+  initAnimation();
+  preloader();
+});
+//# sourceMappingURL=app.js.map
