820c46aa3e18a495161921bd5f28ce722689530b
ilkome <ilya.komichev@gmail.com>
2016-08-03 12:58:15 +0300

    Update config ftp

--- gulpfile.js/config-ftp.js
+++ gulpfile.js/config-ftp.js
@@ -1,10 +1,6 @@
 module.exports = {
-  // host: 'ilko.me',
-  // user: 'user',
-  // password: 'superpasword',
-  // dest: '/' // root folder
-  host: 'style-nes.myjino.ru',
-  user: 'style-nes_gulp',
-  password: '152914666',
+  host: 'ilko.me',
+  user: 'user',
+  password: 'superpasword',
   dest: '/'
 }
