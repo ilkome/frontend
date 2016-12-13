ddd8e77568702668d869cbaf8d6b2acf8e4d83e9
ilkome <ilya.komichev@gmail.com>
2016-12-13 15:03:42 +0300

    Fix error handler

--- gulpfile.js/showToaster.js
+++ gulpfile.js/showToaster.js
@@ -4,6 +4,6 @@ const plumber = require('gulp-plumber')
 module.exports = name => plumber({
   errorHandler: notify.onError(error => ({
     title: name,
-    message: error.filename
+    message: error
   }))
 })
